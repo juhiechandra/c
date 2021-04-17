cac6328f0c1d5ce45e8682bd96be726d8cdb7e04
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-04-17 10:55:59 +0530

    commit

---
+++ Tutorial-10-problem.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// THERE IS SOME ERROR IN THIS CODE
+int main()
+{
+    int pass;
+    printf("Either pass in maths and science or not?");
+
+    scanf("%s", &pass);
+    printf("You have entered %s in maths and science\n", &pass);
+     if (pass = pass)
+     {
+         printf("You will be awarded.");
+     }
+    else
+    {
+        printf("You will never be awarded.");
+    }
+
+    return 0;
+}
