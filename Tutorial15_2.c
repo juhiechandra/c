fca6e6aa5c251872e2d393c5bdd970a9edbb0e99
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-04-25 04:33:02 +0530

    commit

---
+++ Tutorial15_2.c
@@ -0,0 +1,15 @@
+// declaring two variables
+
+#include <stdio.h>
+
+int main()
+{
+    printf("studying for loops\n");
+    int i, j = 0;
+    for (i = 0; i < 5; i++ )
+    {
+        printf("%d %d\n", i, j);
+        ;j++;
+    }
+    return 0;
+}
--- add.c
+++ add.c
@@ -2,12 +2,15 @@
 
 int main()
 {
-    int a, b;
+    int a, b, sum;
     printf("Enter number a");
     scanf("%d", &a);
 
     printf("Enter number b");
     scanf("%d", &b);
+
+    sum = a+b;
+    printf("the sum is %d\n", sum);
 
     return 0;
 }
