3919324bc9350b422017d032394c76bcc72d8384
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-04-27 21:09:08 +0530

    commit

---
+++ ASSIGNMENTS/week2/assignment2/Q3.c
@@ -0,0 +1,15 @@
+// <3>Print all odd number and summation of all odd numbers up to n (hint-:loop and if else).
+
+#include <stdio.h>
+int main()
+{
+    int N;
+    char A;
+   
+    printf("Enter the value of N:\n\n");
+    scanf("%d", &N);
+    printf("For %d value of N, the sum is:", A);
+    A = (N * (N + 1))/ 2;
+    return 0;
+}
+
