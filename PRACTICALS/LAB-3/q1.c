91ebe206aae86b4805062f332101ffd9e3af2759
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-07-15 01:34:54 +0530

    commit

--- PRACTICALS/LAB-3/q1.c
+++ PRACTICALS/LAB-3/q1.c
@@ -1,12 +1,5 @@
-// WAP(Write A Program)
-// to print ‘See C is Sea’ five times on the console.
+// WAP to accept a number and print all its digits in words using Switch and loop statement.(Hint:Input : 149, Output
+//                                                                                           : One Four Nine)
 
 #include<stdio.h>
-int main(){
-    int i;
-    for (i = 0; i < 5; i++)
-    {
-       printf("See C ia Sea\n");
-    }
-    return 0;
-}
+int main
