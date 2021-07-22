7c270af6675e546836ce4a073d0b3f3f291cbc12
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-07-22 18:38:02 +0530

    commit

--- PRACTICALS/LAB-4/Q2.c
+++ PRACTICALS/LAB-4/Q2.c
@@ -1,26 +1,27 @@
-// Q2.WAP to find the sum of the following series from 1 to n using user - defined function.1 ! / 1 + 2 ! / 2 + 3 ! / 3 + …..+ n ! / n.For example, 1 ! / 1 + 2 ! / 2 + 3 ! / 3 + 4 ! / 4 + 5 ! / 5 = 1 + 1 + 2 + 6 + 24 = 34
-
 #include <stdio.h>
+int fact(int);
+void main()
+{
+    int i, n;
+    float sum = 0;
+
+    printf("Enter the value: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum += fact(i) / i;
+    }
+
+    printf("The sum of the series is : %f\n\n", sum);
+}
 int fact(int n)
 {
-    if (n == 1 || n == 0)
+    int num = 0, f = 1;
+    while (num <= n - 1)
     {
-        return 1;
+        f = f + f * num;
+        num++;
     }
-    else
-    {
-        return (n*fact(n - 1));
-    }
-}
-int main()
-{
-    int num, i, sum = 0;
-    printf("Enter the sum upto which you want to print sum:\n");
-    scanf("%d", &num);
-    for (i = 1; i < num; i++)
-    {
-        long x=fact(i)/i;
-        sum = sum + x;
-    }
-    printf("%d", sum);
+    return f;
 }
