babf6726b1a4e02675e7ef03de8b34c839744a84
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-08-08 11:58:03 +0530

    commit

--- VIVA/Q1.c
+++ VIVA/Q1.c
@@ -1,17 +1,19 @@
 // 3. Write programs to print following pattern
-// 			1			
-// 		A 	B	C		
-// 	2	3	4	5	6	
+// 			1
+// 		A 	B	C
+// 	2	3	4	5	6
 // D	E	F	G	H	I	J
 
 #include <stdio.h>
 int main()
 {
     int n;
+    printf("Enter number of rows:", &n);
     scanf("%d", &n);
     int t = n - 1;
     int no = 1;
     int al = 0;
+
     for (int i = 0; i < n; i++)
     {
         for (int i = 0; i < t; i++)
