1b6c9d7479bdbeebda1225626358d1f0707a2c9e
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-08-11 03:03:59 +0530

    1
    1

--- C++/Programiz/1.cpp
+++ C++/Programiz/1.cpp
@@ -9,3 +9,22 @@
 // }
 
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int divisor, dividend, quotient, remainder;
+
+    cout<< "Enter dividend: ";
+    cin >> dividend;
+    cout << "Enter divisor: ";
+    cin>>divisor;
+
+    quotient = dividend/divisor;
+    remainder= dividend%divisor;
+
+    cout << "Quotient = " << quotient << endl;
+    cout << "Remainder = "<< remainder;
+
+    return 0;
+}
