7074208e4fd215c310fd82a91c658f49dc3a6bee
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-08-10 17:37:46 +0530

    jj

---
+++ C++/FREECODECAMP/p3.cpp
@@ -0,0 +1,23 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int luckyNums[] = {4,5,6,4,3,32,67};
+//     cout << luckyNums[2];
+//     return 0;
+// }
+
+
+#include<iostream>
+using namespace std;
+
+void sayHi()
+{
+    cout << "Hello User!";
+}
+
+int main()
+{
+    sayHi();
+    return 0;
+}
