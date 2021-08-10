8e3eddd5ff678eb36435cfa251562cfd61aa4440
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-08-10 17:21:09 +0530

    commit

--- C++/FREECODECAMP/p2.cpp
+++ C++/FREECODECAMP/p2.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 
 using namespace std;
 int main()
@@ -9,5 +9,25 @@ int main()
     cout << "Enter the second number:\n";
     cin >> num2;
     cout << num1 + num2;
+    return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string color, pluralNoun, celebrity;
+    cout << "Enter a color: ";
+    getline(cin, color);
+    cout << "Enter a plural noun: ";
+    getline(cin, pluralNoun);
+    cout << "Enter a celebrity: ";
+    getline(cin, celebrity);
+
+    cout << "Roses are " << color << endl;
+    cout << pluralNoun << " are blue" << endl;
+    cout << "I love " << celebrity << endl;
     return 0;
 }
