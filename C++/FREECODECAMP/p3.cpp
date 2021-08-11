ea94604dc5b0878825ae4d35d3b182b0b265ae50
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-08-11 14:28:23 +0530

    commit

--- C++/FREECODECAMP/p3.cpp
+++ C++/FREECODECAMP/p3.cpp
@@ -1,14 +1,28 @@
+// // #include<iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int luckyNums[] = {4,5,6,4,3,32,67};
+// //     cout << luckyNums[2];
+// //     return 0;
+// // }
+
+// // CALLING FUNCTION SINGLE TIME
 // #include<iostream>
 // using namespace std;
+
+// void sayHi()
+// {
+//     cout << "Hello User!";
+// }
+
 // int main()
 // {
-//     int luckyNums[] = {4,5,6,4,3,32,67};
-//     cout << luckyNums[2];
+//     sayHi();
 //     return 0;
 // }
 
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void sayHi()
