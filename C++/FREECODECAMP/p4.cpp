7ec92008979e8320bc5bc8f67056432e38855940
JUHIE CHANDRA <juhiechandra@gmail.com>
2021-08-19 12:14:16 +0530

    commit

---
+++ C++/FREECODECAMP/p4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void sayHi(string name, int age)
+{
+    cout << "Hello " << name << " you are" << age << endl;
+}
+
+int main()
+{
+    sayHi("j", 34);
+    sayHi("h", 34);
+    sayHi("v", 34);
+}
